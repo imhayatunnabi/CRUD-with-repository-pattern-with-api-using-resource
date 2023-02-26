27319036fe52d3729d6d1ed6be1441175af36574
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2023-02-27 00:52:15 +0600

    index blade ready

---
+++ resources/views/backend/pages/product/index.blade.php
@@ -0,0 +1,32 @@
+@extends('backend.master')
+@section('content')
+<a href="{{ route('backend.product.create') }}" class="btn btn-primary">Create Product</a>
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Price</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $item)
+            <tr class="">
+                <td scope="row">{{ $item->id }}</td>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->quantity }}</td>
+                <td>{{ $item->price }}</td>
+                <td>
+                    <a href="{{ route('backend.product.edit',$item->id) }}" class="btn btn-success">Edit</a>
+                    <a href="{{ route('backend.product.delete',$item->id) }}" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+@endsection
